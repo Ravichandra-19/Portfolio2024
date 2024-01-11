728a673ac0951c70aad68419fcee9b88d8723c23
ravichandra-19 <sairavichandraadavipallii1997@gmail.com>
2024-01-11 15:21:23 +0530

    intial message

--- portfolio2024/src/App.jsx
+++ portfolio2024/src/App.jsx
@@ -1,12 +1,15 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router';
-import Header from './components/header/header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
 function App() {
  
   return (
-    <div className="app">Hello
+    <div className="app">
+      <Header/>
+      
         <Router>
         <Routes>
         <Route/>
+      
         </Routes></Router>
         </div>
   )
